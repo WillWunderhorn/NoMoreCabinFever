7204252751925506da7ea2902d411fdca583732e
Wunderhorn <Narushev_pr1.19@st.ithub.ru>
2023-10-30 19:56:55 +0300

    first fix

---
+++ Patches.cs
@@ -0,0 +1,24 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using HarmonyLib;
+
+namespace NoMoreCabinFever
+{
+    class Implementation : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            //...
+        }
+
+        [HarmonyPatch(typeof(CabinFever), nameof(CabinFever.UpdateCabinFeverTimeTracker))]
+        internal class CabinFever_UpdateCabinFeverTimeTracker
+        {
+            public static void Prefix(ref CabinFever __instance)
+            {
+                __instance.CabinFeverEnd();
+                __instance.ClearCabinFeverRisk();
+            }
+        }
+    }
+}2
